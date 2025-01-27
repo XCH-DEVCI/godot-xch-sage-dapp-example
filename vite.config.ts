edf7c96a37e2efc9f6450a0cec84368a31d37439
XCH-DEVCI <sbcservice22217102@outlook.com>
2025-01-27 13:15:40 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Bind to all network interfaces
-    port: 3000 // Optional: specify a port
-  }
 })
