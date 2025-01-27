8a617c117489bb075664afefb4be5935c0031fc2
TermCIC <sbcservice22217102@outlook.com>
2025-01-27 13:29:09 +0700

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+        output: {
+            manualChunks: {
+                vendor: ['react', 'react-dom'], // Split React-related code into a separate chunk
+            },
+        },
+    },
+    chunkSizeWarningLimit: 100000, // Optional: Adjust warning limit
+},
 })
